66277cd57ea6f50dc07df68f4384c1164c436ce4
doori30 <rublju0070@naver.com>
2019-09-04 16:28:12 +0900

    pug

--- app2.js
+++ app2.js
@@ -38,7 +38,44 @@ app.get(["/page","/page/:page"],(req,res)=>{
 	var css = "page";
 	var js = "page";
 	var vals={page, title, css, js};
-	res.render("page",vals);//page라는 폴더에 vals라는 변수를 page,title을 담아서 보냄.
+	res.render("page",vals);  //page.pug파일에 vals({page, title, css, js})라는 변수를 담아서 보냄.
+});
+
+app.get(["/gbook","/gbook/:type"],(req,res)=>{
+	var type = req.params.type;
+	var pug;
+	var vals = {
+		css:"gbook",
+		js:"gbook"
+	}
+	switch(type){
+		case "in":
+			vals.title = "방명록 작성";
+			pug = "gbook_in";
+			res.render(pug,vals);
+			break;
+			default:
+				var sql = "SELECT * FROM gbook ORDER BY id DESC" 
+				sqlExec(sql).then((data)=>{
+					vals.datas = data[0];
+					vals.title="방명록";
+					pug = "gbook";
+					for(let item of data[0]) item.wtime = util.dspDate(new Date(item.wtime));	
+					res.render(pug,vals);
+				}).catch(sqlErr);
+				//모든필드를 셀렉트함.gbook으로 부터 내림차순으로 정렬(query문)		/ASC는 오름차순, DESC는 내림차순		
+				break;
+	}
 });
 
 //router 영역-POST
+app.post("/gbook_save", (req, res) => {
+	const comment = req.body.comment;
+	const sql = "INSERT INTO gbook SET comment=?, wtime=?"; 
+	//? 안에 들어갈 내용을 const data = await connect.query(sql, vals); 에서 받아서 실행해줌.
+	const vals = [comment, util.dspDate(new Date())];
+	sqlExec(sql, vals).then((data) => {//promise를 리턴 불러오는 개체.구체적으로 편하게 찾기위해 사용하는 함수가 then
+		console.log(data);               //콜백을 simple하게 만들어줌. mysql에 대한 결과값을 보여줌.
+		res.redirect("/gbook");          //input에 글남긴후 다시 gbook으로 돌아옴.
+	}).catch(sqlErr);
+});
